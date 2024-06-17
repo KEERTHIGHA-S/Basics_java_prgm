c422b88781874c0416869deb965a47aa1aae3cf5
KEERTHIGHA-S <159118835+KEERTHIGHA-S@users.noreply.github.com>
2024-06-17 18:27:31 +0530

    Add files via upload

---
+++ ifandif.java
@@ -0,0 +1,21 @@
+public class ifandif {
+	public static void main(String[] args)
+	{
+	int a=6;
+	if(a>3)
+	{
+		a++;
+	}
+	 if(a<10)
+	{
+		a++;
+	}
+	 if(a==8)
+	 {
+		 a++;
+	 }
+	 
+	System.out.println(a);
+}
+	
+}
