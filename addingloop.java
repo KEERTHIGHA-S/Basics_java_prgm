f2a7f6f49b6a806fe1c98d1364d376a6c097bce1
KEERTHIGHA-S <159118835+KEERTHIGHA-S@users.noreply.github.com>
2024-06-17 17:27:09 +0530

    Add files via upload

---
+++ addingloop.java
@@ -0,0 +1,14 @@
+public class addingloop{
+    public static void main(String[] args)
+    {
+        int a=0;
+        int i=5;
+        while(i<=10)
+        {
+            a=a+i;
+            i=i+1;
+            System.out.println(a);
+        }
+        
+    }
+}
