918b6b88f53ef4b6afdb94d960f6fcdf1cd4e16b
KEERTHIGHA-S <159118835+KEERTHIGHA-S@users.noreply.github.com>
2024-06-19 15:15:15 +0530

    Update and rename pattern.py to secondlargest.java

---
+++ secondlargest.java
@@ -0,0 +1,21 @@
+public class secondlarge
+{
+	public static void main(String arug[])
+	{
+		int a=15;
+		int b=20;
+		int c=10;
+		if(a<b && a>c)
+		{
+			System.out.println(a);
+	}
+	else if(b>c)
+	{
+		System.out.println(b);
+	}
+		else
+		{
+			System.out.println(c);
+		}
+	}
+}
