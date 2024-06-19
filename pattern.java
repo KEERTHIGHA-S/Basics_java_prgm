70ff8b39f43c6c88385ca10bdf4698b26d9094e3
KEERTHIGHA-S <159118835+KEERTHIGHA-S@users.noreply.github.com>
2024-06-19 14:57:24 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,14 @@
+public class pattern {
+
+	public static void main(String[] args) {
+ int k;
+		for(int i=1;i<=10;i++)
+		{
+			for(k=1;k<=i;k++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
